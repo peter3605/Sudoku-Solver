51a9d83bf195bb6e6d57275569f6daeffdbc0f1f
Peter Ho <peter.ho433@gmail.com>
2020-05-30 14:11:36 -0400

    Updated basic strategy

--- src/BasicStrategy.java
+++ src/BasicStrategy.java
@@ -10,31 +10,23 @@ public class BasicStrategy extends Strategy{
 	}
 	
 	public boolean solve() {
-		boolean stop = true;
-		int row = 0;
-		int col = 0;
-		for (int i = 0; i < 9; i++) {
-			for (int j = 0; j < 9; j++) {
-				if (grid[i][j] == 0) {
-					stop = false;
-					row = i;
-					col = j;
-					break;
-				}
-			}
-		}
-
-		if (stop) 
-			return true;
-
-		for (int i = 0; i < 10; i++) {
-			if (validate(row, col, i)) {
-				solution[row][col] = i;
-				if (solve())
-					return true;
-				solution[row][col] = 0;
-			}
-		}
-		return false;
+	    for (int i = 0; i < 9; i++){
+	        for (int j = 0; j < 9; j++){
+	            if (solution[i][j] != 0) {
+	                continue;
+	            }
+	 
+	            for (int k = 1; k <= 9; k++) {
+	                solution[i][j] = k;
+	                if(validate(i, j) && solve()) {
+	                    return true;
+	                }
+	                solution[i][j] = 0;
+	            }
+	            return false;
+	        }
+	    }
+	    return true;
 	}
+	
 }
